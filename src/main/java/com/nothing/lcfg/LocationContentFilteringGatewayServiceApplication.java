e3bcd4ea9a41dfa0f66bb3e1d8e186689d04051f
Samuel Brifo <lunarhive@gmail.com>
2024-06-20 08:01:48 +0000

    scaffolding

---
+++ src/main/java/com/nothing/lcfg/LocationContentFilteringGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nothing.lcfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationContentFilteringGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationContentFilteringGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nothing/lcfg/LocationContentFilteringGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nothing.lcfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationContentFilteringGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
